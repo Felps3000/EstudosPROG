69e5b397b9e43a7ec6216a8afec027b0d1622081
f e l p s <filipe.snk@gmail.com>
2023-12-26 13:01:13 -0300

    Initial commit

--- Prova 5/Questão 6.py
+++ Prova 5/Questão 6.py
@@ -22,4 +22,17 @@ Saída	|	Saída	|	Saída	|	Saída	|	Saída
 23
 29
 31
+
+Entrada Saída | Entrada Saída | Entrada Saída | Entrada Saída | Entrada Saída
+11      2     | 8       2     | 1       2     | 3       2     | 6       2
+        3     |         3     |               |         3     |         3
+        5     |         5     |               |         5     |         5
+        7     |         7     |               |               |         7
+        11    |         11    |               |               |         11
+        13    |         13    |               |               |         13
+        17    |         17    |               |               |
+        19    |         19    |               |               |
+        23    |               |               |               |
+        29    |               |               |               |
+        31    |               |               |               |
 """

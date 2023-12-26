5fab4199c334ce2aa80ae736275b2dd7428906dc
f e l p s <filipe.snk@gmail.com>
2023-12-26 13:01:50 -0300

    Initial commit

--- Prova 5/Questão 6.py
+++ Prova 5/Questão 6.py
@@ -7,22 +7,6 @@ Entrada: um valor inteiro positivo.
 Saída: uma sequencia com todos os números primos menores que o valor informado na entrada.
 Exemplos de entrada e saída:
 
-Entrada	|	Entrada	|   Entrada	|	Entrada	|	Entrada
-11		|	8		|	1		|	3		|	6
-        |           |           |           |
-Saída	|	Saída	|	Saída	|	Saída	|	Saída
-2			2			2			2			2
-3			3						3			3
-5			5						5			5
-7			7									7
-11			11									11
-13			13									13
-17			17
-19			19
-23
-29
-31
-
 Entrada Saída | Entrada Saída | Entrada Saída | Entrada Saída | Entrada Saída
 11      2     | 8       2     | 1       2     | 3       2     | 6       2
         3     |         3     |               |         3     |         3

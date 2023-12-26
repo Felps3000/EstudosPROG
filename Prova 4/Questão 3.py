e940eccfcba161a5b1496d4481e640a0816de8fe
f e l p s <filipe.snk@gmail.com>
2023-12-26 12:36:07 -0300

    Initial commit

---
+++ Prova 4/Questão 3.py
@@ -0,0 +1,23 @@
+"""
+3) Um número primo é aquele que possui exatamente dois divisores: 1 e o próprio número. Construa um programa
+que leia um valor inteiro positivo e, a partir de estratégias de repetições, mostra todos os números primos menores
+que esse valor (não existe número primo negativo!), conforme os exemplos abaixo:
+
+Entrada: um valor inteiro positivo.
+Saída: uma sequência com todos os números menores que o valor informado na entrada.
+Exemplos de entrada e saída:
+
+Entrada Saída | Entrada Saída | Entrada Saída | Entrada Saída | Entrada Saída
+35      2     | 20      2     | 2             | 7       2     | 15      2
+        3     |         3     |               |         3     |         3
+        5     |         5     |               |         5     |         5
+        7     |         7     |               |               |         7
+        11    |         11    |               |               |         11
+        13    |         13    |               |               |         13
+        17    |         17    |               |
+        19    |         19    |               |
+        23    |               |               |
+        29    |               |               |
+        31    |               |               |
+"""
+
